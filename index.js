002ab024c3193fd43de84cbe864128ddf42193cd
Eduard-Konovka <ed098ua@gmail.com>
2022-01-30 13:57:37 +0200

    Adds comments

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-const fs = require("fs/promises");
-const path = require("path");
+const fs = require("fs/promises"); // Подключаем встроенную библиотеку работы с файловой системой
+const path = require("path"); // Подключаем встроенную библиотеку нормализации путей к файлам
 
-const numbersPath = path.join("10m.txt");
+const numbersPath = path.join("10m.txt"); // Подключаем файл
 
 async function listNumbers() {
-  return await fs.readFile(numbersPath, "utf-8");
+  return await fs.readFile(numbersPath, "utf-8"); // Считываем файл и приводим его к строке в формате UTF-8
 }
 
 const findValues = (data) => {
   const numbersArr = data
-    .split(/[^-0-9]/)
-    .filter((elem) => elem.trim())
-    .filter((elem) => isFinite(elem))
-    .map((elem) => Number(elem));
-  const sortNumbers = [...numbersArr].sort((a, b) => a - b);
+    .split(/[^-0-9]/) // Превращаем данные в массив разбив строку по всем символам кроме "-" и чисел
+    .filter((elem) => elem.trim()) // Обрезаем пустые символы элементов массива
+    .filter((elem) => isFinite(elem)) // Удаляем елементы массива "-" ("минус" без цифр)
+    .map((elem) => Number(elem)); // Приводим елементы массива к числу
+  const sortNumbers = [...numbersArr].sort((a, b) => a - b); // Сортируем массив по возрастанию
 
   console.log(
     "Максимальное число в файле: ",
-    sortNumbers[sortNumbers.length - 1]
+    sortNumbers[sortNumbers.length - 1] // Выбираем последний елемент массива
   );
 
-  console.log("Минимальное число в файле: ", sortNumbers[0]);
+  console.log("Минимальное число в файле: ", sortNumbers[0]); // Выбираем первый елемент массива
 
   const median =
-    sortNumbers.length % 2 === 0
+    sortNumbers.length % 2 === 0 // Определяем чётность длины массива
       ? (sortNumbers[sortNumbers.length / 2 - 1] +
           sortNumbers[sortNumbers.length / 2]) /
-        2
-      : sortNumbers[(sortNumbers.length - 1) / 2];
+        2 // Чётное значение - суммируем два серединных елемента и делим пополам
+      : sortNumbers[(sortNumbers.length - 1) / 2]; // Нечётное значение - выводим серединный елемент
   console.log("Медиана: ", median);
 
   const arithmeticMean =
-    numbersArr.reduce((acc, elem) => acc + elem, 0) / sortNumbers.length;
+    numbersArr.reduce((acc, elem) => acc + elem, 0) / sortNumbers.length; // Суммируем все элементы и делим на длину массива
   console.log("Среднее арифметическое значение: ", arithmeticMean);
 
   const ascendingSequence = () => {
-    let sequenceArr = [];
-    let sequence = 0;
+    let sequenceArr = []; // Создаём массив значений идущих подряд чисел
+    let sequence = 0; // Создаём счётчик значения отдельной последовательности
 
     [...numbersArr].sort((a, b) => {
       if (a > b) {
-        sequence++;
+        sequence++; // Если число больше предыдущего - увеличиваем счётчик
       } else {
-        sequenceArr.push(sequence);
-        sequence = 0;
+        sequenceArr.push(sequence); // Иначе - добавляем предыдущее значение счётчика в массив значений
+        sequence = 0; // Обнуляем счётчик для подсчёта новой увеличивающейся последовательности
       }
     });
 
-    sequenceArr.push(sequence);
+    sequenceArr.push(sequence); // Добавляем последнее значение счётчика в массив значений
 
-    const sortSequence = sequenceArr.sort((a, b) => b - a);
-    return sortSequence[0];
+    const sortSequence = sequenceArr.sort((a, b) => b - a); // Сортируем массив по убыванию
+    return sortSequence[0]; // Возвращаем первый элемент - самое большое число массива
   };
   console.log(
     "Наибольшая последовательность идущих подряд чисел, которая увеличивается: ",
@@ -58,22 +58,22 @@ const findValues = (data) => {
   );
 
   const descendingSequence = () => {
-    let sequenceArr = [];
-    let sequence = 0;
+    let sequenceArr = []; // Создаём массив значений идущих подряд чисел
+    let sequence = 0; // Создаём счётчик значения отдельной последовательности
 
     [...numbersArr].sort((a, b) => {
       if (a < b) {
-        sequence++;
+        sequence++; // Если число меньше предыдущего - увеличиваем счётчик
       } else {
-        sequenceArr.push(sequence);
-        sequence = 0;
+        sequenceArr.push(sequence); // Иначе - добавляем предыдущее значение счётчика в массив значений
+        sequence = 0; // Обнуляем счётчик для подсчёта новой уменьшающейся последовательности
       }
     });
 
-    sequenceArr.push(sequence);
+    sequenceArr.push(sequence); // Добавляем последнее значение счётчика в массив значений
 
-    const sortSequence = sequenceArr.sort((a, b) => b - a);
-    return sortSequence[0];
+    const sortSequence = sequenceArr.sort((a, b) => b - a); // Сортируем массив по убыванию
+    return sortSequence[0]; // Возвращаем первый элемент - самое большое число массива
   };
   console.log(
     "Наибольшая последовательность идущих подряд чисел, которая уменьшается: ",
@@ -82,7 +82,7 @@ const findValues = (data) => {
 };
 
 listNumbers()
-  .then((result) => findValues(result))
+  .then((result) => findValues(result)) // Оперируем данными из файла
   .catch((err) => {
-    console.log(err.message);
+    console.log(err.message); // Выводим сообщение ошибки
   });
